4487d7e728405502594a4c3553ae96d74c9569a6
Eren Akel <eren.akel.46@gmail.com>
2021-11-16 21:24:16 +0300

    The products fetched from commerce.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
-
+import { commerce } from './lib/commerce';
 import { Products, Navbar } from './components';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products)
+
   return (
     <div className="App">
       <Navbar />
-      <Products />
+      <Products products={products} />
     </div>
   );
 }
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -8,19 +8,17 @@ const Product = ({ product }) => {
 
     return (  
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image={product.image} title={product.name}/>
+            <CardMedia className={classes.media} image={product.image.url} title={product.name} />
             <CardContent>
-                <div className={classes.CardContent}>
-                    <Typography variant="h5" gutterBottom>
+                <div className={classes.cardContent}>
+                    <Typography variant="h5" gutterBottom component="h2">
                         {product.name}
                     </Typography>
-                    <Typography variant="h5" gutterBottom>
-                        {product.price}
+                    <Typography variant="h5" gutterBottom component="h2">
+                        {product.price.formatted_with_symbol}
                     </Typography>
                 </div>
-                <Typography variant="body2" color="textSecondary">
-                    {product.description}
-                </Typography>
+                <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
             </CardContent>
             <CardActions disableSpacing className={classes.CardActions} >
                 <IconButton aria-label="Add to Cart">
